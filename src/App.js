c1052ecc5e2d2de1567e07915414b6b5b5d11351
Samuel <samuel.y.yamaguchi@gmail.com>
2021-06-23 00:52:06 -0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 
 import Home from './pages/Home.js';
