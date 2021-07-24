d5b1b73e9b379311ebdd115e0c7f5ebffd54132e
Samuel <samuel.y.yamaguchi@gmail.com>
2021-07-24 15:45:18 -0300

    oopsie

---
+++ src/pages/Author.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { colorTheme } from "../util/colorTheme";
+
+class Author extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render = () => {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Author;
