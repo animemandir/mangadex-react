f11f0330176d9c3c24d679c811924a23fa4d72d4
Samuel <samuel.y.yamaguchi@gmail.com>
2022-01-31 00:19:08 -0300

    i hope it's now fine

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     style: {
-        postcss: {
+        postcssOptions: {
             plugins: [
                 require('tailwindcss'),
                 require('autoprefixer'),
             ],
         },
-    },
-  }
+    }
+}
